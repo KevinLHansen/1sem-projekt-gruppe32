6d5ab202d598c844d9eebc51b6cfcb96dc0eaa90
SimonHanel <mc-mc-simon@hotmail.com>
2018-10-23 14:28:17 +0200

    Added setInfos to rooms
    Plus comments to the methods in the Game class.

--- worldofzuul/Game.java
+++ worldofzuul/Game.java
@@ -15,15 +15,15 @@ public class Game
 
     private void createRooms()
     {
+        //Adding instances of rooms with descriptions.
         Room foyer, livingRoom, diningRoom, kitchen, staircase, secondFloor, attic, basement, masterBedroom, porch, nwGarden, nGarden, neGarden, wGarden, swGarden, seGarden, treehouse;
         
-        
-        foyer = new Room("Foyer");
+        foyer = new Room("Foyer - Front entrance");
         livingRoom = new Room("Living room");
         diningRoom = new Room("Dining room");
         kitchen = new Room("Kitchen");
         staircase = new Room("Staircase");
-        secondFloor = new Room("2nd floor");
+        secondFloor = new Room("Second floor corridor");
         attic = new Room("Attic");
         basement = new Room("Basement");
         masterBedroom = new Room("Master bedroom - Mom and dad's bedroom");
@@ -36,51 +36,65 @@ public class Game
         seGarden = new Room("South east gardens - South eastern outside area");
         treehouse = new Room("Treehouse - Northern outside area");
         
+        //Setting exits and infos to rooms.
+        //Infos are called through the "examine" command for the current room that the player currently is located.
         foyer.setExit("livingroom", livingRoom);
         foyer.setExit("outside", porch);
         foyer.setExit("upstairs", staircase);
         foyer.setExit("diningroom", diningRoom);
+        foyer.setInfo("I can put my toy cars here...");
         
         livingRoom.setExit("foyer", foyer);
+        livingRoom.setInfo("I can put some christmas ornaments by the window...");
         
         diningRoom.setExit("foyer", foyer);
         diningRoom.setExit("kitchen", kitchen);
+        diningRoom.setInfo("I could setup a trap here with glue, a fan and some feathers...");
         
         kitchen.setExit("basement", basement);
         kitchen.setExit("diningroom", diningRoom);
         kitchen.setExit("outside", neGarden);
+        kitchen.setInfo("I should get ready for when the crooks arrive. Buzz' BB gun could be useful if they decide to enter the backdoor... \nI could set up a blowtorch trap here...");
         
         staircase.setExit("up", secondFloor);
         staircase.setExit("down", foyer);
+        staircase.setInfo("I should set up at least one trap here, so that they won't get upstairs without a fight...\nMaybe the buckets of paint could work out...");
         
         secondFloor.setExit("masterbedroom", masterBedroom);
         secondFloor.setExit("attic", attic);
         // secondFloor.setExit("room", room); 
         // Adding more rooms later depending on the items required and immersive experience.
+        secondFloor.setInfo("Upstairs. Maybe a tripwire between the narrow hallway could slow them down...");
         
         attic.setExit("hallway", secondFloor);
+        attic.setInfo("The attic is the perfect way for a zipline escape route to my treehouse! My dad has some rope laying around somewhere...");
         
         basement.setExit("kitchen", kitchen);
         basement.setExit("outside", neGarden);
+        basement.setInfo("There's tonnes of stuff down here I can use to set up traps.\nMaybe I could set up a trap with a nail and some tar on the steps. Or even switch out the lightbulb switch with a trapdoor...\nI could also use my dad's heater on the front doorknob.");
         
         masterBedroom.setExit("hallway", secondFloor);
+        masterBedroom.setInfo("Mom's and Dad's bedroom. There's a phone here I can use to call the cops when the crooks are inside the house...");
         
         porch.setExit("inside", foyer);
         porch.setExit("west", swGarden);
         porch.setExit("east", seGarden);
+        porch.setInfo("There's nothing much here, but the door is very undefended. I could pour water over the stairs so that they slip when they try to get in...");
         
         nwGarden.setExit("east", nGarden);
         nwGarden.setExit("south", wGarden);
         //nwGarden.setExit("window", livingRoom); 
-        // Marv-only movement "idea"
+        // Marv-only movement "idea" for GUI iteration of the game
         
         nGarden.setExit("treehouse", treehouse);
         nGarden.setExit("west", nwGarden);
         nGarden.setExit("east", neGarden);
+        nGarden.setInfo("My treehouse is nearby. Could make for an excellent escape route from the attic...");
                 
         neGarden.setExit("west", nGarden);
         neGarden.setExit("basement", basement);
         neGarden.setExit("kitchen", kitchen);
+        neGarden.setInfo("I know the crooks are trying to come through the kitchen door first.\nThe garden hose could help me set up an ice-slippery trap to the basement.");
         
         wGarden.setExit("north", nwGarden);
         wGarden.setExit("south", swGarden);
@@ -89,10 +103,16 @@ public class Game
         swGarden.setExit("east", porch);
         
         seGarden.setExit("west", porch);
+        seGarden.setInfo("There's a bucket here. I can fill it with water and pour the water onto the steps at the front porch...");
         
+        treehouse.setExit("down", nGarden);
+        treehouse.setInfo("I need to set up an escape route here from the attic. My dad has some rope lying around...");
+        
+        //Setting starting-point to be inside at the front door, after Kevin returns from church and prepares his traps.
         currentRoom = foyer;
     }
 
+    //Play method to start the game
     public void play() 
     {            
         printWelcome();
@@ -102,7 +122,7 @@ public class Game
         while (! finished) {
             Command command = parser.getCommand();
             finished = processCommand(command);
-            boolean objective1Complete = false;
+            boolean objective1Complete = false; //Not in use yet, but will be crucial for when we write our win-conditions.
             if (objective1Complete == true){
                 finished = true;
             }
@@ -110,6 +130,7 @@ public class Game
         System.out.println("Thank you for playing. Good bye.");
     }
 
+    //Welcome screen for when the player runs the program
     private void printWelcome()
     {
         System.out.println();
@@ -120,6 +141,7 @@ public class Game
         System.out.println(currentRoom.getLongDescription());
     }
 
+    //Command processing, checking for a command and executing the associated method if the command exists.
     private boolean processCommand(Command command) 
     {
         boolean wantToQuit = false;
@@ -152,6 +174,7 @@ public class Game
         return wantToQuit;
     }
         
+    //Checks if a room has a setInfo that contains more than "", and prints the info.
     private void printInfo(String info)
     {
         if(info == "") {
@@ -162,7 +185,7 @@ public class Game
         }
     }
     
-    
+    //Method used for calling the "help"-command that prints out instructions and commands.
     private void printHelp() 
     {
         System.out.println("You are lost. You are alone. You wander");
@@ -172,6 +195,7 @@ public class Game
         parser.showCommands();
     }
 
+    //Method used for walking between rooms and checks for any invalid rooms.
     private void goRoom(Command command) 
     {
         if(!command.hasSecondWord()) {
@@ -192,6 +216,7 @@ public class Game
         }
     }
 
+    //Quit program method
     private boolean quit(Command command) 
     {
         if(command.hasSecondWord()) {
