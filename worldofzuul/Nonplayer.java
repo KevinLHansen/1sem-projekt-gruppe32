11bc4883232334bc3aa3c0f559af4c251e481174
Mathias Jakobsen <mjako18@student.sdu.dk>
2018-11-06 12:11:11 +0100

    Update worldofzuul/Nonplayer.java
    Added a start to the pathing for the NPCs.

--- worldofzuul/Nonplayer.java
+++ worldofzuul/Nonplayer.java
@@ -1,13 +1,28 @@
 package worldofzuul;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Needs methods specific to the wet bandits, Harry and Marv
+ *
  * @author Mathias
  */
 public class Nonplayer extends Creature {
-    
+
+    private List<Room> pathList;
+    private Room[] path;
+
     public Nonplayer(String name) {
         super(name);
+        this.pathList = new ArrayList<>();
     }
-    
+
+    public void setPathList(Room room) {
+        this.pathList.add(room);
+    }
+
+    public void setPath() {
+        this.path = this.pathList.toArray(new Room[0]);
+    }
 }
