c8043b4f1a425aec7512c90be8de89361e19e042
hedenjacob <43139424+hedenjacob@users.noreply.github.com>
2018-11-06 12:54:07 +0100

    createItems

--- worldofzuul/Room.java
+++ worldofzuul/Room.java
@@ -1,61 +1,55 @@
 package worldofzuul;
 
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
+public class Room {
 
-public class Room 
-{
     private String description;
     private HashMap<String, Room> exits;
     private boolean trapSet;
     private String info;
 
-    public Room(String description) 
-    {
+    public Room(String description) {
         this.description = description;
-        this.info ="";
+        this.info = "";
         this.trapSet = false;
         exits = new HashMap<String, Room>();
     }
 
-    public void setExit(String direction, Room neighbor) 
-    {
+    public void setExit(String direction, Room neighbor) {
         exits.put(direction, neighbor);
     }
 
-    public String getShortDescription()
-    {
+    public String getShortDescription() {
         return description;
     }
 
-    public String getLongDescription()
-    {
+    public String getLongDescription() {
         return "You are at/in the " + description + ".\n" + getExitString();
     }
 
-    private String getExitString()
-    {
+    private String getExitString() {
         String returnString = "Exits: ";
         Set<String> keys = exits.keySet();
-        for(String exit : keys) {
+        for (String exit : keys) {
             returnString += exit + ", ";
         }
         return returnString;
     }
 
-    public Room getExit(String direction) 
-    {
+    public Room getExit(String direction) {
         return exits.get(direction);
     }
-    
+
     public void setInfo(String info) {
         this.info = info;
     }
 
     public String getInfo() {
         return info;
-    }   
+    }
 }
-
