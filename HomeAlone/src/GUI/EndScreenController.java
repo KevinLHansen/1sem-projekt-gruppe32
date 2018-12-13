5aeb4cb6273d67abc8df441633655ac031d91342
hedenjacob <43139424+hedenjacob@users.noreply.github.com>
2018-12-13 10:35:26 +0100

    fixy

--- HomeAlone/src/GUI/EndScreenController.java
+++ HomeAlone/src/GUI/EndScreenController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package GUI;
 
 import HomeAlone.business.Game;
@@ -17,7 +12,6 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
